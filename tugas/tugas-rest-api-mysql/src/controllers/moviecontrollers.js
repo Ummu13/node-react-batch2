3b73b35591f1e1174b3d0c7e61fd2e9d3592e277
Ummu13 <ummuradiah13@gmail.com>
2025-10-15 15:44:32 +0800

    mengerjakan tugas day 7

--- tugas/tugas-rest-api-mysql/src/controllers/moviecontrollers.js
+++ tugas/tugas-rest-api-mysql/src/controllers/moviecontrollers.js
@@ -27,61 +27,72 @@ const createNilaiMahasiswa = (req, res) => {
   })
 }
 
-// //read movie (menampilkan semua data)
-// const readMovie= (req, res) => {
-//   let queryText = "SELECT * FROM movie"
-//   connectionPool.query(queryText, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-// res.json(data);
-// });
-// }
+ //read movie (menampilkan semua data)
+ const readNilaiMahasiswa= (req, res) => {
+   let queryText = "SELECT * FROM nilai_mahasiswa"
+   connectionPool.query(queryText, (err, data) => {
+     if (err) {
+       console.error(err);
+       return;
+     }
+ res.json(data);
+ });
+ }
 
-// //read movie by id (menampilkan data berdasarkan id)
-// const readMovieById= (req, res) => {
-//   let {id} = req.params 
-//   let queryText= `SELECT * FROM movie WHERE id =${id}`
-//   connectionPool.query(queryText, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-// res.json(data);
-// });
-// }
+//read movie by id (menampilkan data berdasarkan id)
+const readNilaiMahasiswaById= (req, res) => {
+  let {id} = req.params 
+  let queryText= `SELECT * FROM nilai_mahasiswa WHERE id =${id}`
+  connectionPool.query(queryText, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+res.json(data);
+});
+}
 
 // //Update data (menampilkan data berdasarkan id)
-// const updateMovie = (req, res) => {
-//   let {title, year} = req.body
-//   let {id} = req.params 
+ const updateNilaiMahasiswa = (req, res) => {
+   let {nama, mata_kuliah, nilai} = req.body
+   let {id} = req.params 
+   
+  if (nilai < 0 || nilai > 100){
+    return res.json({'message': 'Nilai harus antara 0-100', 'status': 'error'})
+  }
+    
+  let indeks_nilai = "";
+  if (nilai >= 80 ) indeks_nilai = "A";
+  else if (nilai >= 70 && nilai < 80  ) indeks_nilai =  "B";
+  else if (nilai >= 60 && nilai < 70 ) indeks_nilai = "C";
+  else if (nilai >= 50 && nilai < 60 ) indeks_nilai = "D";   
+  else indeks_nilai = "E";  
 
-//   let queryText= `Update movie Set title="${title}", year=${year}, updated_at=now() WHERE id =${id}`
-//   connectionPool.query(queryText, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     res.json({message: 'Succesfully Updated', status: 'success'})
-// res.json(data);
-// });
-// }
+   let queryText= `Update nilai_mahasiswa Set nama="${nama}", mata_kuliah="${mata_kuliah}", nilai=${nilai}, indeks_nilai="${indeks_nilai}",updated_at=NOW() WHERE id =${id}`
+   connectionPool.query(queryText, (err, data) => {
+     if (err) {
+       console.error(err);
+       return;
+     }
+     res.json({message: 'Succesfully Updated', status: 'success'})
+ res.json(data);
+ });
+ }
 
 // //Hapus data (menghapus data didatabase berdasarkan id)
-// const deleteMovie = (req, res) => {
-//   let {id} = req.params 
-//   let queryText= `DELETE FROM movie WHERE id =${id}`
+const deleteNilaiMahasiswa = (req, res) => {
+  let {id} = req.params 
+  let queryText= `DELETE FROM nilai_mahasiswa WHERE id =${id}`
   
-//   connectionPool.query(queryText, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     res.json({message: 'DELETE Succesfully', status: 'success'})
-// res.json(data);
-// });
-// }
+  connectionPool.query(queryText, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.json({message: 'DELETE Succesfully', status: 'success'})
+res.json(data);
+});
+}
 
-module.exports = {createNilaiMahasiswa}
+module.exports = {createNilaiMahasiswa, readNilaiMahasiswa, readNilaiMahasiswaById, updateNilaiMahasiswa, deleteNilaiMahasiswa}
 //module.exports = {getMoviesByID}
--- tugas/tugas-rest-api-mysql/src/routers/main.js
+++ tugas/tugas-rest-api-mysql/src/routers/main.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const {createNilaiMahasiswa} = require("../controllers/moviecontrollers");
+const {createNilaiMahasiswa, readNilaiMahasiswa, readNilaiMahasiswaById, updateNilaiMahasiswa, deleteNilaiMahasiswa} = require("../controllers/moviecontrollers");
 const router = express.Router();
 
 //router.get("/movie/:id", getMoviesByID);
 
 router.post('/mahasiswa', createNilaiMahasiswa)
-// router.get('/movie', readMovie)
-// router.get('/movie/:id', readMovieById)
-// router.put('/movie/:id', updateMovie)
-// router.delete('/movie/:id', deleteMovie)
+router.get('/mahasiswa', readNilaiMahasiswa)
+router.get('/mahasiswa/:id', readNilaiMahasiswaById)
+router.put('/mahasiswa/:id', updateNilaiMahasiswa)
+router.delete('/mahasiswa/:id', deleteNilaiMahasiswa)
 
 module.exports = { router };
 
